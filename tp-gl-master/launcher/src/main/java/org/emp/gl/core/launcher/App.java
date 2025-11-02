ab340630633fad84e5688c2cd4bdacc3bd2b71cc
LARBI <larbisalahedine@gmail.com>
2025-11-02 16:48:30 +0100

    exception when cd-5 for example get to 0

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -2,7 +2,9 @@ package org.emp.gl.core.launcher;
 
 import org.emp.gl.timer.service.impl.DummyTimeServiceImpl;
 import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.clients.CompteARebours;
 import org.emp.gl.clients.Horloge;
+import java.util.Random;
 
 public class App {
 
@@ -11,12 +13,25 @@ public class App {
     }
 
     private static void testDuTimeService() {
-        // 1. Créer le TimerService
+        // 1. Créer le TimerService (une seule fois)
         TimerService service = new DummyTimeServiceImpl();
 
-        // 2. Injecter le service dans la ou les Horloges
+        // 2. Créer des horloges
         Horloge horloge1 = new Horloge("Num 1", service);
         Horloge horloge2 = new Horloge("Num 2", service);
+
+        // 3. Créer un compte à rebours de 5 secondes
+        CompteARebours c = new CompteARebours("CD-5", 5, service);
+
+        // 4. Créer plusieurs comptes à rebours aléatoires
+        Random rnd = new Random();
+        for (int i = 1; i <= 10; i++) {
+            int seconds = 10 + rnd.nextInt(11); // entre 10 et 20
+            new CompteARebours("CD-" + i, seconds, service);
+        }
+
+        // 5. Si ton service doit être démarré manuellement :
+        // ((DummyTimeServiceImpl) service).start(); // décommente si nécessaire
     }
 
     public static void clearScreen() {
---
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/CompteARebours.java
@@ -0,0 +1,57 @@
+package org.emp.gl.clients;
+
+import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.timer.service.TimerChangeListener;
+
+/**
+ * CompteARebours : similaire à Horloge, mais décrémente une valeur entière chaque seconde.
+ */
+public class CompteARebours implements TimerChangeListener {
+    private final String name;
+    private final TimerService timerService;
+    private int remaining;             // nombre de secondes restantes
+    private boolean active = true;     // utilisé si on préfère marquer inactif avant suppression
+
+    public CompteARebours(String name, int initialSeconds, TimerService timerService) {
+        this.name = name;
+        this.remaining = initialSeconds;
+        this.timerService = timerService;
+
+        // S'enregistrer auprès du service
+        this.timerService.addTimeChangeListener(this);
+        System.out.println(name + " démarré avec " + initialSeconds + "s");
+    }
+
+    @Override
+    public void propertyChange(String propertyName,Object oldValue, Object newValue) {
+        // ici on suppose que SECONDE_PROP est le nom envoyé chaque seconde
+        if (!active) return;
+
+        if (TimerChangeListener.SECONDE_PROP.equals(propertyName)) {
+            tick();
+        }
+    }
+
+    private synchronized void tick() {
+        if (!active) return;
+        if (remaining <= 0) return;
+
+        remaining--;
+        System.out.println(name + " -> " + remaining + "s");
+
+        if (remaining <= 0) {
+            // Option 1 : se désinscrire immédiatement
+            timerService.removeTimeChangeListener(this);
+            active = false;
+            System.out.println(name + " : terminé et désinscrit.");
+        }
+    }
+
+    // méthode utilitaire si on veut forcer l'arrêt
+    public synchronized void stop() {
+        if (!active) return;
+        active = false;
+        timerService.removeTimeChangeListener(this);
+        System.out.println(name + " : arrêté manuellement et désinscrit.");
+    }
+}
